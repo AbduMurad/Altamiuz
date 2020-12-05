a37f85d70105c4f45ad42262d92617ee1770e909
AbduMurad <abadeem@gmail.com>
2020-12-05 08:51:39 +0200

    fix services directory import in index

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,12 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Header from '../components/header/header'
-import ServicesSection from "../components/services/ServicesSection"
+import ServicesSection from "../components/Services/ServicesSection"
 import OurVisionSection from "../components/our-vision/OurVisionSection"
 import TrainingSection from "../components/training/TrainingSection"
 import CoursesSection from "../components/courses/CoursesSection"
 import CeoQoute from "../components/stakeholders/CeoQoute"
 import Partners from "../components/stakeholders/Partners"
-
 import ContactUs from "../components/contact-us/ContactUs"
 
 const IndexPage = () => (
